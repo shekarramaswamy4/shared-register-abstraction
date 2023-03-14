b7811f44f357c2f7bebf32a5ee1d1e5ee6b3ed47
Shekar Ramaswamy <shekar@slide.so>
2023-03-13 18:53:48 -0700

    Test three nodes at once

--- client/client_test.go
+++ client/client_test.go
@@ -26,3 +26,29 @@ func TestInitialization(t *testing.T) {
 	n1.Server.Close()
 	c.Server.Close()
 }
+
+func Test3Nodes(t *testing.T) {
+	n1 := node.New(8080)
+	n2 := node.New(8081)
+	n3 := node.New(8082)
+
+	c := New(8070, 3, 8080)
+
+	go n1.StartHTTP()
+	go n2.StartHTTP()
+	go n3.StartHTTP()
+	go c.StartHTTP()
+	time.Sleep(1)
+
+	err := c.Write("addr1", "val1")
+	assert.Nil(t, err)
+	v, err := c.Read("addr1")
+	assert.Nil(t, err)
+	assert.Equal(t, "val1", v.Value)
+	assert.Equal(t, 1, v.Version)
+
+	n1.Server.Close()
+	n2.Server.Close()
+	n3.Server.Close()
+	c.Server.Close()
+}
